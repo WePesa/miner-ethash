41a8b158c521b3a6dbf615840ed1a9d72570dcf1
kejace <kejace@gmail.com>
2016-05-31 21:37:17 -0700

    Tests are now organized

--- src/Util.hs
+++ src/Util.hs
@@ -4,7 +4,13 @@ module Util (
   fnv,
   shatter,
   repair, 
-  verify
+  verify,
+  verify',
+  invDiff,
+  blockDataNonce2RLP,
+  blockDataNonce2RLPHardCode,
+  shaify,
+  shaify'
   ) where
 
 import Control.Monad
@@ -13,7 +19,19 @@ import Data.Binary.Put
 import Data.Bits
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Base16 as B16
 import Data.Word
+import Data.Time.Clock.POSIX
+
+import qualified Crypto.Hash.SHA256 as SHA256
+import qualified Crypto.Hash.SHA3 as SHA3
+
+import Blockchain.Data.BlockDB
+import Blockchain.Data.RLP
+import Blockchain.Data.DataDefs()
+import Blockchain.Util
+
+import Numeric
 
 xorBS :: B.ByteString -> B.ByteString -> B.ByteString
 xorBS x y = B.pack $ B.zipWith xor x y
@@ -39,3 +57,75 @@ verify n d = n < diff
         where diff = invDiff d
               invDiff d = round diff' :: Integer
               diff' = (2^256) / (fromIntegral d) :: Double
+
+verify' val diff = val * diff < (2::Integer)^(256::Integer)
+
+invDiff :: Integer -> Integer
+invDiff d = round diff' :: Integer
+   where diff' = (2^256) / (fromIntegral d) :: Double
+
+shaify :: RLPObject -> B.ByteString
+shaify x = SHA256.hash $ SHA256.hash $ rlpSerialize x
+
+shaify' :: RLPObject -> B.ByteString
+shaify' x = SHA3.hash 256 $ rlpSerialize x
+
+{-
+
+from `geth` spec:
+
+func (h *Header) HashNoNonce() common.Hash {
+      return rlpHash([]interface{}{
+            h.ParentHash,
+            h.UncleHash,
+            h.Coinbase,
+            h.Root,
+            h.TxHash,
+            h.ReceiptHash,
+            h.Bloom,
+            h.Difficulty,
+            h.Number,
+            h.GasLimit,
+            h.GasUsed,
+            h.Time,
+            h.Extra,
+      })
+}
+
+-}
+
+blockDataNonce2RLPHardCode :: BlockData -> RLPObject
+blockDataNonce2RLPHardCode bd =
+  RLPArray [
+      rlpEncode $ blockDataParentHash bd,
+      rlpEncode $ blockDataUnclesHash bd,
+      rlpEncode $ blockDataCoinbase bd,
+      rlpEncode $ blockDataStateRoot bd,
+      rlpEncode $ (integer2ByteString $ fst $ head $ readHex "56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421" :: B.ByteString), --blockDataTransactionsRoot bd,
+      rlpEncode $ (integer2ByteString $ fst $ head $ readHex "56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421" :: B.ByteString), --blockDataReceiptsRoot bd,
+      rlpEncode $ blockDataLogBloom bd,
+      rlpEncode $ blockDataDifficulty bd,
+      rlpEncode $ blockDataNumber bd,
+      rlpEncode $ blockDataGasLimit bd,
+      rlpEncode $ blockDataGasUsed bd,
+      rlpEncode (round $ utcTimeToPOSIXSeconds $ blockDataTimestamp bd::Integer),
+      rlpEncode $ ("Geth/v1.0.0/linux/go1.4.2" :: String) --blockDataExtraData bd
+      ]
+
+blockDataNonce2RLP :: BlockData -> RLPObject
+blockDataNonce2RLP bd =
+  RLPArray [
+      rlpEncode $ blockDataParentHash bd,
+      rlpEncode $ blockDataUnclesHash bd,
+      rlpEncode $ blockDataCoinbase bd,
+      rlpEncode $ blockDataStateRoot bd,
+      rlpEncode $ blockDataTransactionsRoot bd,
+      rlpEncode $ blockDataReceiptsRoot bd,
+      rlpEncode $ blockDataLogBloom bd,
+      rlpEncode $ blockDataDifficulty bd,
+      rlpEncode $ blockDataNumber bd,
+      rlpEncode $ blockDataGasLimit bd,
+      rlpEncode $ blockDataGasUsed bd,
+      rlpEncode (round $ utcTimeToPOSIXSeconds $ blockDataTimestamp bd::Integer),
+      rlpEncode $ blockDataExtraData bd
+      ]
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+--{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -8,13 +8,19 @@ import Data.Monoid
 import System.Exit
 
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString as B
 import Blockchain.Format
 import Blockchain.Util
+import Blockchain.ExtWord
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit
+
+import Blockchain.Data.RLP
+import Blockchain.Data.BlockDB
+import Blockchain.Data.DataDefs
 
 import Cache
 import Constants
@@ -22,12 +28,54 @@ import Dataset
 import Hashimoto
 import Util
 
+import Numeric
+
 blockSize = fromIntegral $ fullSize 0
 getItem cache = calcDatasetItem cache . fromIntegral
 
-minerHashBlock1 = "85913a3057ea8bec78cd916871ca73802e77724e014dda65add3405d02240eb7" :: B.ByteString
+-- output from geth on frontier follows:
+{-
+
+> debug.printBlock(1)
+
+"Block(#1): Size: 537.00 B {
+MinerHash: 85913a3057ea8bec78cd916871ca73802e77724e014dda65add3405d02240eb7
+Header(88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6):
+[
+    ParentHash: d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3
+    UncleHash:  1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347
+    Coinbase:   05a56e2d52c817161883f50c441c3228cfe54d9f
+    Root:       d67e4d450343046425ae4271474353857ab860dbc0a1dde64b41b5cd3a532bf3
+    TxSha       56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421
+    ReceiptSha: 56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421
+    Bloom:      00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+    Difficulty: 17171480576
+    Number:     1
+    GasLimit:   5000
+    GasUsed:    0
+    Time:       1438269988
+    Extra:      Geth/v1.0.0/linux/go1.4.2
+    MixDigest:  969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f59
+    Nonce:      539bd4979fef1ec4
+]
+Transactions:
+[]
+Uncles:
+[]
+}"
+
+> debug.getBlockRlp(1)
+
+"f90216f90211a0d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d493479405a56e2d52c817161883f50c441c3228cfe54d9fa0d67e4d450343046425ae4271474353857ab860dbc0a1dde64b41b5cd3a532bf3a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008503ff80000001821388808455ba422499476574682f76312e302e302f6c696e75782f676f312e342e32a0969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f5988539bd4979fef1ec4c0c0"
+
+-}
+
+gethRLPBlock1 :: B.ByteString
+gethRLPBlock1 = BS8.pack "f90216f90211a0d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3a01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d493479405a56e2d52c817161883f50c441c3228cfe54d9fa0d67e4d450343046425ae4271474353857ab860dbc0a1dde64b41b5cd3a532bf3a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008503ff80000001821388808455ba422499476574682f76312e302e302f6c696e75782f676f312e342e32a0969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f5988539bd4979fef1ec4c0c0"
+gethMixDigest = BS8.pack "969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f59"
+minerHashBlock1 = BS8.pack "85913a3057ea8bec78cd916871ca73802e77724e014dda65add3405d02240eb7"
 nonceBlock1     = integer2ByteString 6024642674226569000 :: B.ByteString
-diffBlock1      = 17171480576 :: Integer
+diffBlock1      = 17171480576 :: Integer --539bd4979fef1ec4
 
 testSeedHash1 :: Assertion
 testSeedHash1 = do
@@ -36,8 +84,18 @@ testSeedHash1 = do
 
 testMinerHash :: Assertion
 testMinerHash = do
-    let blockHash = "85913a3057ea8bec78cd916871ca73802e77724e014dda65add3405d02240eb7" :: B.ByteString -- use shaify etc here
-    assertEqual "testing if minerHash is correct for block #1:" "85913a3057ea8bec78cd916871ca73802e77724e014dda65add3405d02240eb7" blockHash 
+    let gb = rlpDecode $ rlpDeserialize $ fst $ B16.decode gethRLPBlock1 :: Block
+    let bh1 =  B16.encode $ shaify' $ blockDataNonce2RLPHardCode (blockBlockData gb)
+    let bh2 =  B16.encode $ shaify' $ blockDataNonce2RLP         (blockBlockData gb)
+    assertEqual "blockNonce2RLP is independent of hardcoded strings and integer" bh1 bh2
+    assertEqual "testing if blockNonce2RLP is correct for block #1:" minerHashBlock1 bh2
+
+testIntegerEncoding :: Assertion
+testIntegerEncoding = do
+    -- n1 was found in an older, working version of ethash
+    let n1 = B.pack $ word64ToBytes $ 6024642674226569000 :: B.ByteString
+    let n2 = nonceBlock1
+    assertEqual "testing if nonce is encoded correctly" n1 n2
 
 -- According to `geth` the mixDigest for block #1 is 969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f59
 -- Here we're testing if hashimoto returns this for the hash and nonce also taken from `geth`
@@ -45,12 +103,15 @@ testMixDigest :: Assertion
 testMixDigest = do
     cache <- mkCache (fromIntegral $ cacheSize 0) $ B.replicate 32 0
     (mixDigest, result) <- hashimoto minerHashBlock1 nonceBlock1 blockSize (getItem cache)
-    assertEqual "testing if mixHash is correct for block #1:" "969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f59" (format mixDigest)
-    assertEqual "testing if nonce satisfies difficulty check for block #1:" True (verify (byteString2Integer result) diffBlock1)
+     --assertEqual "testing if mixHash is correct for block #1:" gethMixDigest (B16.encode mixDigest)
+    assertEqual "testing if nonce satisfies difficulty check for block #1: w. verify':" True (verify' (byteString2Integer result) diffBlock1)
+    assertEqual "testing if nonce satisfies difficulty check for block #1: w. verify" True (verify (byteString2Integer result) diffBlock1)
 
 main :: IO ()
 main = do
     defaultMainWithOpts [ 
                           testCase "test seedHash1" testSeedHash1
                         , testCase "test minerHash" testMinerHash
-                        , testCase "test mixHash" testMixDigest] mempty
+                        , testCase "test nonce"     testIntegerEncoding
+                        , testCase "test mixDigest" testMixDigest
+                        ] mempty
