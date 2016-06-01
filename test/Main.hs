cef601b253ce6ddcd03f119ca0ca0bb8ca7eb101
kejace <kejace@gmail.com>
2016-05-31 17:22:47 -0700

    Organizing tests

--- test/Main.hs
+++ test/Main.hs
@@ -32,9 +32,6 @@ diffBlock1      = 17171480576 :: Integer
 testSeedHash1 :: Assertion
 testSeedHash1 = do
     assertEqual "testing if epoch 1 is correct" "0x0000000000000000000000000000000000000000000000000000000000000000" "0x0000000000000000000000000000000000000000000000000000000000000000"
-
-testSeedHash2 :: Assertion
-testSeedHash2 = do
     assertEqual "testing epoch 30000 is correct" "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563" "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
 
 testMinerHash :: Assertion
@@ -42,30 +39,18 @@ testMinerHash = do
     let blockHash = "85913a3057ea8bec78cd916871ca73802e77724e014dda65add3405d02240eb7" :: B.ByteString -- use shaify etc here
     assertEqual "testing if minerHash is correct for block #1:" "85913a3057ea8bec78cd916871ca73802e77724e014dda65add3405d02240eb7" blockHash 
 
-
 -- According to `geth` the mixDigest for block #1 is 969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f59
 -- Here we're testing if hashimoto returns this for the hash and nonce also taken from `geth`
-testMixDigest :: Cache -> Assertion
-testMixDigest cache = do
+testMixDigest :: Assertion
+testMixDigest = do
+    cache <- mkCache (fromIntegral $ cacheSize 0) $ B.replicate 32 0
     (mixDigest, result) <- hashimoto minerHashBlock1 nonceBlock1 blockSize (getItem cache)
     assertEqual "testing if mixHash is correct for block #1:" "969b900de27b6ac6a67742365dd65f55a0526c41fd18e1b16f1a1215c2e66f59" (format mixDigest)
-
-testBlock1 :: Cache -> Assertion
-testBlock1 cache = do
-    (mixDigest, result) <- hashimoto minerHashBlock1 nonceBlock1 blockSize (getItem cache)
     assertEqual "testing if nonce satisfies difficulty check for block #1:" True (verify (byteString2Integer result) diffBlock1)
-
-testNumber :: Assertion
-testNumber = do
-    let n = 20::Integer
-    assertEqual "rlp encoding failed for small number" (n) n
 
 main :: IO ()
 main = do
-    cache <- mkCache (fromIntegral $ cacheSize 0) $ B.replicate 32 0
     defaultMainWithOpts [ 
                           testCase "test seedHash1" testSeedHash1
-                        , testCase "test seedHash2" testSeedHash2
                         , testCase "test minerHash" testMinerHash
-                        , testCase "test mixHash" (testMixDigest cache)
-                        , testCase "test nonce for block #1" (testBlock1 cache)] mempty
+                        , testCase "test mixHash" testMixDigest] mempty
